d926f483c9e940b3c313bfc4f0e74b0219fe2d26
Ben M <bsmukasa@gmail.com>
2015-10-22 13:59:13 -0400

    Add model skeletons.

--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Loan(models.Model):
+    pass
+
+
+class LoanSnapShot(models.Model):
+    pass
+
+
+class Portfolio(models.Model):
+    pass
