94d8572bfde0a3834a5a51e64564851199953e6c
Ben M <bsmukasa@gmail.com>
2015-10-22 11:39:00 -0400

    Add gitignore file.

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(
+        regex=r'^admin/',
+        view=include(admin.site.urls)
+    ),
 ]
