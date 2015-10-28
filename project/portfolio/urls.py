5924b846646d3bfe5a8bd6dd7e2b313234431107
Ben M <bsmukasa@gmail.com>
2015-10-28 15:17:20 -0400

    Add create and get portfolio url patterns.

--- project/portfolio/urls.py
+++ project/portfolio/urls.py
@@ -1,1 +1,18 @@
-__author__ = 'benmukasa'
+from django.conf.urls import patterns, url
+from portfolio import views
+
+urlpatterns = patterns(
+    '',
+
+    url(
+        regex=r'^create_portfolio$',
+        view=views.PortfolioAPI.as_view(),
+        name='create_portfolio_api'
+    ),
+
+    url(
+        regex=r'^get_portfolios/$',
+        view=views.PortfolioAPI.as_view(),
+        name='get_portfolios_api'
+    ),
+)
