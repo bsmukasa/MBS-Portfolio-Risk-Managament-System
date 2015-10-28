9155b0f6224f488e02413cdfaf12cfe1708362a9
Ben M <bsmukasa@gmail.com>
2015-10-28 19:00:56 -0400

    Add model skeletons.

--- project/risk_management/models.py
+++ project/risk_management/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class RiskProfile(models.Model):
+    pass
+
+
+class RiskFactor(models.Model):
+    pass
+
+
+class AssumptionProfile(models.Model):
+    pass
+
+
+class ScoreCard(models.Model):
+    pass
+
+
+class ScoreCardAttribute(models.Model):
+    pass
+
+
+class ScoreCardAdjustments(models.Model):
+    pass
+
+
+class Scenario(models.Model):
+    pass
+
