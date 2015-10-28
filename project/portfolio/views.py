7a5589d61c9d6ee28f72980abd3ba34192b4d2a0
Ben M <bsmukasa@gmail.com>
2015-10-28 13:35:49 -0400

    Update Portfolio and Loan apis.

--- project/portfolio/views.py
+++ project/portfolio/views.py
@@ -9,6 +9,17 @@ class PortfolioAPI(View):
     model = Portfolio
 
     def get(self, request):
+        """ Gets all a users loan portfolios meeting given filter values in the request.GET.
+
+        The active user is retrieved using the user_id stored in the session. If the user exists,
+        use filter factors such as count for pagination or filter values to get relevant
+        portfolios.
+
+        User must be logged.
+
+        :param request: Request; must include name
+        :return: Json object with portfolios.
+        """
         user = get_user_model().objects.filter(pk=request.session['user_key'])
         if user.exists():
             filter_dict = request.GET.dict()
@@ -16,14 +27,57 @@ class PortfolioAPI(View):
             user_portfolios = self.model.objects.filter(**filter_dict).values()
             return JsonResponse(dict(profiles=list(user_portfolios)))
 
+    def post(self, request):
+        """ Creates and saves a portfolio given a portfolio name.
+
+        :param request: Request; must include portfolio name.
+        """
+        user = get_user_model().objects.filter(pk=request.session['user_key'])
+        if user.exists():
+            name = request.POST.get('name', '')
+            new_portfolio = self.model(name=name)
+            new_portfolio.user = user
+
+            # TODO Add loans to portfolio from csv file.
+            loan_file = request.POST.get('loan_file', '')
+
+            # TODO Calculate portfolio numbers from loaded loans.
+            new_portfolio.total_loan_balance = 0
+            new_portfolio.total_loan_count = 0
+            new_portfolio.average_loan_balance = 0
+            new_portfolio.weighted_average_coupon = 0
+            new_portfolio.weighted_average_life_to_maturity = 0
+
 
 class LoanAPI(View):
     model = Loan
 
     def get(self, request):
+        """ Gets all the loans from a selected portfolio in the session using
+        filter values in the request.GET.
+
+        Portfolio is retrieved using the portfolio_id stored in the session. If the portfolio exists,
+        use filter factors such as a count of loans for pagination or field values to get loans
+        meeting specific values.
+
+        :param request: Request.
+        :return: Json object with loans.
+        """
         portfolio = Portfolio.objects.filter(pk=request.session['portfolio_id'])
         if portfolio.exists():
             filter_dict = request.GET.dict()
             filter_dict['portfolio'] = portfolio
             profile_loans = self.model.objects.filter(**filter_dict).values()
             return JsonResponse(dict(loans=list(profile_loans)))
+
+
+# class LoanAdjustedAssumptionsAPI(View):
+#     model = LoanAdjustedAssumptions
+#
+#     # TODO Determine how adjustments to loans are calculated, in aggregate or individually.
+#     def post(self, request):
+#         portfolio = Portfolio.objects.filter(pk=request.session['portfolio_id'])
+#         if portfolio.exists():
+#             filter_dict = request.GET.dict()
+#             filter_dict['portfolio'] = portfolio
+#             affected_loans = self.model.objects.filter(**filter_dict).values()
