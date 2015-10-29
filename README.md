42efa0015671d7a33a0e8a7cad418dc854964157
Juliana Kawase <ju.kawase@gmail.com>
2015-10-29 13:29:33 -0400

    Add User Model with Login and Signup

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Development Setup
 1. Install and activate a virtual environment.
 2. Install and update all librarys in django-toolbelt.
+3. Install package django-bootstrap-form
--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from front.views import Index, Login, Signup, Dashboard
+from accounts.views import Index, Login, Signup, Dashboard, Contact
 
 
 urlpatterns = patterns(
@@ -9,4 +9,5 @@ urlpatterns = patterns(
 	url(r'^login/$', Login.as_view(), name="login"),
 	url(r'^signup/$', Signup.as_view(), name="signup"),
 	url(r'^dashboard/$', Dashboard.as_view(), name="dashboard"),
+	url(r'^contact/$', Contact.as_view(), name="contact"),
 	)
