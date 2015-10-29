c43acc3a98152630d538e3e91b1ff98cb1918051
Ben M <bsmukasa@gmail.com>
2015-10-29 14:26:53 -0400

    Add get and post methods to RiskProfileAPI.

--- project/risk_management/views.py
+++ project/risk_management/views.py
@@ -1,3 +1,5 @@
+import json
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
@@ -13,10 +15,19 @@ class RiskProfileAPI(View):
         return super(RiskProfileAPI, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        pass
+        filter_dict = request.GET.dict()
+        risk_profiles = self.model.objects.filter(**filter_dict).values()
+        return JsonResponse(dict(risk_profiles=list(risk_profiles)))
 
     def post(self, request):
-        pass
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        name = body['name']
+
+        new_risk_profile = self.model(name=name)
+
+        new_risk_profile.save()
+        return JsonResponse({'status': 'OK', 'message': 'Risk Profile Created!!'})
 
 
 class RiskFactorAPI(View):
