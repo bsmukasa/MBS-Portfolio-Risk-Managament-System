432c4ec35619b463f820e06d7a356002d050e8eb
Ben M <bsmukasa@gmail.com>
2015-10-29 14:14:04 -0400

    Add RiskProfileAPI and RiskFactorAPI class skeletons.

--- project/risk_management/views.py
+++ project/risk_management/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+from risk_management.models import RiskProfile, RiskFactor
+
 
 # Create your views here.
+class RiskProfileAPI(View):
+    model = RiskProfile
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(RiskProfileAPI, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class RiskFactorAPI(View):
+    moodel = RiskFactor
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(RiskFactorAPI, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
