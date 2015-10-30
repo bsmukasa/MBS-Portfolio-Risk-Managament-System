2ea1f62fc60a2f293c33db40ccbe63f19aca4d95
Ben M <bsmukasa@gmail.com>
2015-10-30 16:10:22 -0400

    Add create_assumption_profile and get_assumption_profiles patterns.

--- project/risk_management/urls.py
+++ project/risk_management/urls.py
@@ -1,1 +1,18 @@
-__author__ = 'benmukasa'
+from django.conf.urls import patterns, url
+from risk_management import views
+
+urlpatterns = patterns(
+    '',
+
+    url(
+        regex=r'^create_assumption_profile$',
+        view=views.AssumptionProfileAPI.as_view(),
+        name='create_assumption_profile_api'
+    ),
+
+    url(
+        regex=r'^get_assumption_profiles$',
+        view=views.AssumptionProfileAPI.as_view(),
+        name='get_assumption_profiles_api'
+    ),
+)
