8398c4ce1cdb927f653b3d1124b4a8f568bf0225
Ben M <bsmukasa@gmail.com>
2015-10-30 12:52:46 -0400

    Add post method to RiskFactorAPI to create risk factor with conditionals.

--- project/risk_management/views.py
+++ project/risk_management/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
-from risk_management.models import RiskProfile, RiskFactor
+from risk_management.models import RiskProfile, RiskFactor, RiskConditional
 
 
 # Create your views here.
@@ -57,4 +57,57 @@ class RiskFactorAPI(View):
             return JsonResponse({'status': 'FAIL', 'message': 'Risk Profile ID must be provided.'})
 
     def post(self, request):
-        pass
+        """ Creates a new risk factor and related conditionals.
+
+        Json in the Request must include:
+        - risk_profile_id
+        - risk_factor_attribute
+        - changing_assumption
+        - percentage_change
+        - conditionals_list
+
+        Example:
+            {
+                "risk_factor": {
+                    "risk_profile_id": 5,
+                    "risk_factor_attribute": "FICO",
+                    "changing_assumption": "CDR",
+                    "percentage_change": "-5",
+                    "conditionals_list": {
+                        "conditional_item": [
+                            {"conditional": ">", "value": 450},
+                            {"conditional": "<", "value": 550}
+                        ]
+                    }
+                }
+            }
+
+
+        :param request: Request.
+        :return: JsonResponse with status and message.
+        """
+        # filter_dict = request.GET.dict()
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+
+        if 'risk_profile_id' in body.keys():
+            risk_profile_id = body['risk_profile_id']
+            risk_profile = RiskProfile.objects.filter(pk=risk_profile_id)
+
+            if risk_profile.exists():
+                new_risk_factor = self.model(risk_profile=risk_profile)
+                new_risk_factor.attribute = body['risk_factor_attribute']
+                new_risk_factor.changing_assumption = body['changing_assumption']
+                new_risk_factor.percentage_change = body['percentage_change']
+
+                conditionals_list = body['conditionals_list']
+                for item in conditionals_list:
+                    new_risk_condtional = RiskConditional(risk_factor=new_risk_factor)
+                    new_risk_condtional.conditional = item['conditional']
+                    new_risk_factor.value = item['value']
+
+                return JsonResponse({'status': 'PASS', 'message': 'Risk Factor added.'})
+            else:
+                return JsonResponse({'status': 'FAIL', 'message': 'Risk Profile provided does not exist.'})
+        else:
+            return JsonResponse({'status': 'FAIL', 'message': 'Risk Profile ID must be provided.'})
