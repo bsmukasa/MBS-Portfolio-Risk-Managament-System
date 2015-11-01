dcaa151288050d1661fcc41dafb1087eb1831d96
Ben M <bsmukasa@gmail.com>
2015-11-01 14:09:34 -0500

    Add create_risk_profiles function.

--- project/project/seeder.py
+++ project/project/seeder.py
@@ -1,28 +1,27 @@
 import os
 import django
-from random import uniform, choice
+from random import uniform
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
 
 from faker import Factory
-from risk_management.models import AssumptionProfile
+from risk_management.models import AssumptionProfile, RiskProfile
 
 fake = Factory.create()
 
 
-def create_assumption_profiles(profile_number=6):
+def create_assumption_profiles():
     assumption_profile_names = [
-        'GDP Growing at 3%', 'GDP Shrinking at 5%', 'Europe Collapse', 'Alien Invasion',
-        'Texas Uprising', 'Timber Price Hike', 'Water Shortage', 'Minimum Wage Rate Hike',
-        'Donald Trump Presidency', 'Bernie Sanders Presidency', 'Zombie Apocalypse', 'HPI Growth at 3%',
-        'Unemployment Growth by 8%', 'Unemployment Shrinkage by 5%', 'HPI Shrinkage by 5%',
-        'HYS Up by 5%', 'HYS Down by 8%'
+        'GDP Growing at 3%', 'GDP Shrinking at 5%', 'Alien Invasion',
+        'Minimum Wage Rate Hike', 'Donald Trump Presidency',
+        'Bernie Sanders Presidency', 'Zombie Apocalypse'
     ]
     count = 0
-    for assumption_profile in range(profile_number):
+
+    for name in assumption_profile_names:
         assumption_profile = AssumptionProfile()
-        assumption_profile.name = choice(assumption_profile_names)
+        assumption_profile.name = name
         assumption_profile.gdp_growth = round(uniform(-6, 6), 4)
         assumption_profile.unemployment_rate = round(uniform(5, -15), 4)
         assumption_profile.national_home_price_index_growth = round(uniform(-20, 20), 4)
@@ -43,7 +42,24 @@ def create_assumption_profiles(profile_number=6):
         assumption_profile.save()
 
 
+def create_risk_profiles():
+    risk_profile_names = [
+        'Zipcodes in NY', 'Zipcodes in NJ', 'Zipcodes in CT', 'Zipcodes in TX', 'Zipcodes in NV',
+        'Zipcodes in IL', 'Zipcodes in FL', 'Zipcodes in AB', 'Zipcodes in OK', 'Zipcodes in CA',
+        'Prequalified FICO', 'Subprime FICO', 'Midrange FICO', 'Less than 5 Years Remaining Term',
+        'Less than 8 Years Remaining Term', 'Less than 10 Years Remaining Term',
+        'Less than 15 Years Remaining Term', 'Less than 20 Years Remaining Term',
+        'Current Interest Rate Above 2%', 'Current Interest Rate Above 5%', 'Current Interest Rate Above 8%'
+    ]
+
+    for name in risk_profile_names:
+        risk_profile = RiskProfile()
+        risk_profile.name = name
+        risk_profile.save()
+
+
 if __name__ == '__main__':
     print("Starting seed script...")
-    create_assumption_profiles()
+    # create_assumption_profiles()
+    # create_risk_profiles()
     print("Seeding complete...")
