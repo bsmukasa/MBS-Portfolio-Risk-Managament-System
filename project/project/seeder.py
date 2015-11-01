a53e29402c4a527a97d14b9fc6666743dd26ea96
Ben M <bsmukasa@gmail.com>
2015-11-01 13:54:37 -0500

    Add assumption_profile_name_list

--- project/project/seeder.py
+++ project/project/seeder.py
@@ -1,6 +1,6 @@
 import os
 import django
-from random import uniform
+from random import uniform, choice
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
@@ -11,11 +11,18 @@ from risk_management.models import AssumptionProfile
 fake = Factory.create()
 
 
-def create_assumption_profiles(profile_number=10):
+def create_assumption_profiles(profile_number=6):
+    assumption_profile_names = [
+        'GDP Growing at 3%', 'GDP Shrinking at 5%', 'Europe Collapse', 'Alien Invasion',
+        'Texas Uprising', 'Timber Price Hike', 'Water Shortage', 'Minimum Wage Rate Hike',
+        'Donald Trump Presidency', 'Bernie Sanders Presidency', 'Zombie Apocalypse', 'HPI Growth at 3%',
+        'Unemployment Growth by 8%', 'Unemployment Shrinkage by 5%', 'HPI Shrinkage by 5%',
+        'HYS Up by 5%', 'HYS Down by 8%'
+    ]
     count = 0
     for assumption_profile in range(profile_number):
         assumption_profile = AssumptionProfile()
-        assumption_profile.name = fake.company()
+        assumption_profile.name = choice(assumption_profile_names)
         assumption_profile.gdp_growth = round(uniform(-6, 6), 4)
         assumption_profile.unemployment_rate = round(uniform(5, -15), 4)
         assumption_profile.national_home_price_index_growth = round(uniform(-20, 20), 4)
@@ -38,5 +45,5 @@ def create_assumption_profiles(profile_number=10):
 
 if __name__ == '__main__':
     print("Starting seed script...")
-    create_assumption_profiles(6)
+    create_assumption_profiles()
     print("Seeding complete...")
