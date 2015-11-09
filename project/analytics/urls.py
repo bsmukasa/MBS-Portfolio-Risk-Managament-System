7dffd241481146b107e059e21bcf073614889a43
Ben M <bsmukasa@gmail.com>
2015-11-09 18:41:53 -0500

    Add get_cash_flows url pattern.

--- project/analytics/urls.py
+++ project/analytics/urls.py
@@ -1,1 +1,12 @@
-__author__ = 'benmukasa'
+from django.conf.urls import patterns, url
+from analytics import views
+
+urlpatterns = patterns(
+    '',
+
+    url(
+        regex=r'^get_cash_flows$',
+        view=views.CashFlowsAPI.as_view(),
+        name='get_cash_flows_api'
+    ),
+)
