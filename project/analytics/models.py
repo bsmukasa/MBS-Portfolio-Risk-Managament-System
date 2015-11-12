a120824238bf0d6ffd590c80de6ee96a12e4e604
bsmukasa <bsmukasa@gmail.com>
2015-11-12 17:12:03 -0500

    Add discount_rate to CashFlowsResults model.

--- project/analytics/models.py
+++ project/analytics/models.py
@@ -8,4 +8,5 @@ class CashFlowsResults(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     scenario = models.ForeignKey(Scenario)
     portfolio = models.ForeignKey(Portfolio)
+    discount_rate = models.DecimalField(max_digits=10, decimal_places=4)
     analysis_results_file_name = models.CharField()
