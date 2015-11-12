1280581874d1176af24051d2d1291b42e5200575
bsmukasa <bsmukasa@gmail.com>
2015-11-12 17:32:36 -0500

    Add AggregateCashFlowsAPI class and get method.

--- project/analytics/views.py
+++ project/analytics/views.py
@@ -6,6 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
+from analytics.adjusted_assumptions_engine import get_adjusted_assumptions
 from analytics.cash_flows_engine import LoanPortfolio
 from analytics.models import CashFlowsResults
 from portfolio.models import Loan
@@ -33,10 +34,8 @@ class CashFlowsAPI(View):
         request_dict = request.POST.dict()
 
         scenario_id = request_dict['scenario_id']
-        portfolio_id = request_dict['scenario_id']
-
-        # TODO Add object that provides adjusted cpr, cdr, recovery and discount rate
-        discount_rate = 0.10
+        portfolio_id = request_dict['portfolio_id']
+        discount_rate = request_dict['discount_rate']
 
         analysis_results_name = "scenario_{}_portfolio_{}_analysis".format(scenario_id, portfolio_id)
 
@@ -64,3 +63,37 @@ class CashFlowsAPI(View):
             message = 'Analysis has already been run.'
 
         return JsonResponse({'status': 'PASS', 'message': message})
+
+
+class AggregateCashFlowsAPI(View):
+    model = CashFlowsResults
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(AggregateCashFlowsAPI, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        request_dict = request.GET.dict()
+
+        scenario_id = request_dict['scenario_id']
+        portfolio_id = request_dict['scenario_id']
+        discount_rate = request_dict['discount_rate']
+        adjusted_assumptions = get_adjusted_assumptions(scenario_id, portfolio_id)
+
+        analysis_results_name = "scenario_{}_portfolio_{}_analysis".format(scenario_id, portfolio_id)
+
+        cash_flow_results = self.model.objects.filter(
+            scenario_id=scenario_id,
+            portfolio_id=portfolio_id,
+            discount_rate=discount_rate,
+            analysis_results_name=analysis_results_name
+        )
+
+        if cash_flow_results.exists():
+            loan_df = pd.read_pickle(analysis_results_name + '_loans.pk')
+            cash_flow_df = pd.read_pickle(analysis_results_name + '_cash_flows.pk')
+
+            analysis_portfolio = LoanPortfolio(discount_rate=discount_rate, loan_df=loan_df, cash_flow_df=cash_flow_df)
+            aggregate_cash_flow_df = analysis_portfolio.cash_flows_aggregate_for_portfolio()
+            aggregate_cash_flow = aggregate_cash_flow_df.to_json(orient="records")
+            return JsonResponse(dict(aggregate_cash_flows=list(aggregate_cash_flow)))
