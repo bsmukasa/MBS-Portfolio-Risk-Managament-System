057a26fc54a6b9923ba4a5727af3fc6f71b299bd
bsmukasa <bsmukasa@gmail.com>
2015-11-12 18:53:15 -0500

    Work in progress.

--- project/analytics/adjusted_assumptions_engine.py
+++ project/analytics/adjusted_assumptions_engine.py
@@ -1,9 +1,30 @@
+import pandas as pd
+
 from portfolio.models import Loan
-from risk_management.models import Scenario
+from risk_management.models import Scenario, RiskFactor
+
+
+class ScoreCard:
+    def __init__(self, assumption_type):
+        self.assumption_type = assumption_type
+        self.attributes = dict()
+        for choice in RiskFactor.RISK_FACTOR_ATTRIBUTE_CHOICES:
+            weight = 100 / len(RiskFactor.RISK_FACTOR_ATTRIBUTE_CHOICES)
+            self.attributes[choice] = dict(weight=weight, index=1.0, original_score=weight)
 
 
 def get_adjusted_assumptions(scenario_id, portfolio_id):
+    score_card_types = ['CDR', 'CPR', 'RECOVERY']
     scenario = Scenario.objects.get(scenario_id=scenario_id)
     assumptions = scenario.assumption_profile
     risk_list = scenario.risk_profiles.all()
+
+    attributes = dict()
+    for choice in RiskFactor.RISK_FACTOR_ATTRIBUTE_CHOICES:
+            weight = 100 / len(RiskFactor.RISK_FACTOR_ATTRIBUTE_CHOICES)
+            attributes[choice] = dict(weight=weight, index=1.0, original_score=weight)
+
+    cdr_df = pd
+
+    loan_df = pd.DataFrame(list(Loan.objects.filter(portfolio_id=portfolio_id)))
 
