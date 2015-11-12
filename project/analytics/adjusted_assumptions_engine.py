a8a65dd5a97ee9f11e65ee731c3b845b7118d7ea
bsmukasa <bsmukasa@gmail.com>
2015-11-12 17:31:43 -0500

    Add get_adjusted_assumptions function.

--- project/analytics/adjusted_assumptions_engine.py
+++ project/analytics/adjusted_assumptions_engine.py
@@ -1,1 +1,10 @@
+from portfolio.models import Loan
+from risk_management.models import Scenario
 
+
+def get_adjusted_assumptions(scenario_id, portfolio_id):
+    scenario = Scenario.objects.get(scenario_id=scenario_id)
+    assumptions = scenario.assumption_profile
+    risks = scenario.risk_profiles
+
+    loans = Loan.objects.get(portfolio_id=portfolio_id)
