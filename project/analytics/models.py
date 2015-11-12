4066ccb19fd141aaa9bf45ea0d6179516db9b46c
bsmukasa <bsmukasa@gmail.com>
2015-11-12 15:39:11 -0500

    Add CashFlowResults model.

--- project/analytics/models.py
+++ project/analytics/models.py
@@ -1,2 +1,11 @@
-# Create your models here.
+from django.db import models
+from risk_management.models import Scenario
+from portfolio.models import Portfolio
 
+
+# Create your models here.
+class CashFlowsResults(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    scenario = models.ForeignKey(Scenario)
+    portfolio = models.ForeignKey(Portfolio)
+    analytics_file_name = models.CharField()
