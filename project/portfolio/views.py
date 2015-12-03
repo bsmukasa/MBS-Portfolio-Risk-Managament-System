b8498a246c974b521132cc49795361ee1300b0ca
bsmukasa <bsmukasa@gmail.com>
2015-12-03 15:14:49 -0500

    Update PortfolioAPI get method to send formatted text.

--- project/portfolio/views.py
+++ project/portfolio/views.py
@@ -1,7 +1,6 @@
 import codecs
 import csv
 import datetime
-import locale
 import random
 
 from django.http import JsonResponse
@@ -38,9 +37,23 @@ class PortfolioAPI(View):
         return super(PortfolioAPI, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        user_portfolios = self.model.objects.all().values()
+        user_portfolios = self.model.objects.all()
+        portfolios_list = []
 
-        return JsonResponse(dict(portfolios=list(user_portfolios)))
+        for portfolio in user_portfolios:
+            portfolios_list.append(
+                {
+                    'total_loan_balance': convert_decimal_to_currency(portfolio.total_loan_balance),
+                    'total_loan_count': convert_int_to_comma_separated(portfolio.total_loan_count),
+                    'average_loan_balance': convert_decimal_to_currency(portfolio.average_loan_balance),
+                    'weighted_average_coupon': convert_decimal_to_percentage(portfolio.weighted_average_coupon),
+                    'weighted_average_life_to_maturity': convert_decimal_to_currency(
+                        portfolio.weighted_average_life_to_maturity
+                    )
+                }
+            )
+
+        return JsonResponse(dict(portfolios=portfolios_list))
 
     def post(self, request):
         form = FileForm(data=request.POST, files=request.FILES)
@@ -163,10 +176,6 @@ class PortfolioAPI(View):
             return JsonResponse({'status': 'OK', 'message': 'Risk Profile Created!!'})
 
 
-def next_bank_loan_id(last_id=None):
-    return last_id + random.randint(151, 1127)
-
-
 class LoanPaginationAPI(View):
     model = Loan
 
@@ -267,6 +276,10 @@ class PortfolioStatusAPI(View):
         return JsonResponse({'data': result}, safe=False)
 
 
+def next_bank_loan_id(last_id=None):
+    return last_id + random.randint(151, 1127)
+
+
 def convert_decimal_to_currency(decimal_number):
     return '{:,.2f}'.format(decimal_number)
 
@@ -277,6 +290,7 @@ def convert_decimal_to_percentage(decimal_number):
 
 def convert_int_to_comma_separated(int_number):
     return '{:,}'.format(int_number)
+
 
 class PortfolioFICOAPI(View):
     @method_decorator(csrf_exempt)
